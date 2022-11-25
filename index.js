fe324f73c38e736068ee53f89f64719722092886
rubayet-islam <season.ss2001@gmail.com>
2022-11-25 23:02:34 +0600

    bikes endpoint created

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Motohub server running');
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tsmlaiu.mongodb.net/test`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tsmlaiu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const productsCollection = client.db("motoHub").collection("bikes");
+
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const products = await productsCollection.find(query).toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+
+run().catch((error) => console.log(error));
+
+app.get("/", (req, res) => {
+  res.send("Motohub server running");
 });
 
 app.listen(port, () => {
-    console.log(`Motohub server running on ${port}`);
+  console.log(`Motohub server running on ${port}`);
 });
