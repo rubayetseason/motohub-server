7296859070e2766330ad42ec06e569f151b9b583
rubayet-islam <season.ss2001@gmail.com>
2022-11-27 21:57:32 +0600

    user delete API created

--- index.js
+++ index.js
@@ -100,6 +100,13 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
