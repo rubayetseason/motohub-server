2cc92d6f67cc533c31e8fd3bbdce63f030689f82
rubayet-islam <season.ss2001@gmail.com>
2022-11-28 12:43:03 +0600

    roles added

--- index.js
+++ index.js
@@ -95,6 +95,20 @@ async function run() {
 
     //users part here
 
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
+    });
+
+    app.get("/users/seller/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isSeller: user?.role === "seller" });
+    });
+
     app.get("/users", async (req, res) => {
       const role = req.query.role;
       let query = {};
